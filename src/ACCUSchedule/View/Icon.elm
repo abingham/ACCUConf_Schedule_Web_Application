c025b63ca60cbc4dd458a9caa45b9371573fc984
Austin Bingham <austin.bingham@gmail.com>
2019-02-27 14:13:42 +0100

    Added icon module.

---
+++ src/ACCUSchedule/View/Icon.elm
@@ -0,0 +1,21 @@
+module ACCUSchedule.View.Icon exposing (icon)
+
+import ACCUSchedule.Msg as Msg
+import Element exposing (Attribute, Element, el, htmlAttribute, text)
+import Element.Events exposing (onClick)
+import Element.Font as Font
+import Html.Attributes exposing (style)
+
+
+icon : List (Attribute Msg.Msg) -> String -> Element Msg.Msg
+icon attrs =
+    let
+        defaultAttrs =
+            [ Font.family [ Font.typeface "Material Icons" ]
+            , htmlAttribute (style "cursor" "default")
+            ]
+
+        fullAttrs =
+            defaultAttrs ++ attrs
+    in
+    text >> el fullAttrs
